170cab54565c0dafa212a62b626fca9a2835592b
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-09-18 23:00:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Countries from './Components/Country/Countries'
+function App() {
+    return (
+        <>
+        <Countries></Countries>
+      </>
+    )
+}
+export default App
---
+++ src/Components/Country/Countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1 className="">hello</h1>
+
+
+        </div>
+    );
+};
+
+export default Countries;
